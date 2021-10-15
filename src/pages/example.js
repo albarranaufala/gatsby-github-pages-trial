0b73ce85fa5324aa42dd4be6f0d664aaba9c19c5
Albarra Erdanto <albarraerdanto@gmail.com>
2021-10-15 14:07:38 +0700

    add example page

---
+++ src/pages/example.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const ExamplePage = () => {
+  return (
+    <main>
+      <h1>This is Example Page</h1>
+      <Link to="/">Back to home</Link>
+    </main>
+  )
+}
+
+export default ExamplePage
